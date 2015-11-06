f0c94d75847ab771345bc37cecf00b444d0ac8cc
Ken Wilcox <wilcoxk@gmail.com>
2015-11-06 00:28:49 -0700

    typo

--- echo.hs
+++ echo.hs
@@ -5,7 +5,7 @@ main :: IO ()
 main = do
     args <- getArgs
     -- display the first two arguments
-    -- use dummy values is none are provided
+    -- use dummy values if none are provided
     let def = args ++ ["world", "woot!"]
     putStrLn ("Hello, " ++ def !! 0 ++ " " ++ def !! 1)
 
