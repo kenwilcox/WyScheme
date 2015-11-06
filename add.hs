b5d7c9489316b0a7e808fe93266b5f7c0b1f7708
Ken Wilcox <wilcoxk@gmail.com>
2015-11-06 00:32:03 -0700

    oops

--- add.hs
+++ add.hs
@@ -4,7 +4,7 @@ import System.Environment
 main :: IO ()
 main = do
     args <- getArgs
-    -- display the first two arguments
+    -- adds the first two arguments
     -- use dummy values if none are provided
     let def = args ++ ["40", "2"]
     print ((read $ def !! 0) + (read $ def !! 1))
