67507afaff7451d317ca6457ae370439ece0c2cc
Ken Wilcox <wilcoxk@gmail.com>
2015-11-06 00:19:10 -0700

    There IS an easier way

--- echo.hs
+++ echo.hs
@@ -4,14 +4,8 @@ import System.Environment
 main :: IO ()
 main = do
     args <- getArgs
-    -- I'm SURE there is an easier way to do this
     -- display the first two arguments
     -- use dummy values is none are provided
-    let arg1 = if length args > 1
-        then args !! 0
-        else "world"
-    let arg2 = if length args > 1
-        then args !! 1
-        else "woot!"
-    putStrLn ("Hello, " ++ arg1 ++ " " ++ arg2)
+    let def = args ++ ["world", "woot!"]
+    putStrLn ("Hello, " ++ def !! 0 ++ " " ++ def !! 1)
 
